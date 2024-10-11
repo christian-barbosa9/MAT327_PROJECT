f7688cf2e748bb7ae1c15edb3e1918e84fc133a0
christian-barbosa9 <cbarbosa603@icloud.com>
2024-10-10 21:53:30 -0400

    Add files via upload

---
+++ M_5.R
@@ -0,0 +1,28 @@
+
+#AGE OF PASSENGERS
+mean_value <- mean(test$Age)
+> median_value <- median(test$Age)
+> variance_value <- var(test$Age)
+> sd_value <- sd(test$Age)
+> mean_value
+[1] 39.62096
+> median_value
+[1] 40
+> variance_value
+[1] 229.089
+> sd_value
+[1] 15.13569
+
+#FLIGHT DISTANCE
+mean_value <- mean(test$`Flight Distance`)
+>  median_value <- median(test$`Flight Distance`)
+>  variance_value <- var(test$`Flight Distance`)
+>  sd_value <- sd(test$`Flight Distance`)
+mean_value
+[1] 1193.788
+> median_value
+[1] 849
+> variance_value
+[1] 997369.7
+> sd_value
+[1] 998.684
