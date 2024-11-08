89f7200133d37453c71f6deeaad9f736ef50a2a3
christian-barbosa9 <cbarbosa603@icloud.com>
2024-11-07 23:23:33 -0500

    Add files via upload

---
+++ M_8.R
@@ -0,0 +1,18 @@
+y <- test$`Arrival Delay in Minutes`
+x <- test$`Departure Delay in Minutes`
+
+
+model <- lm(y ~ x)
+
+
+summary(model)
+
+
+residuals <- model$residuals
+
+
+hist(residuals, breaks=30, main="Histogram of Residuals", xlab="Residuals", col="lightblue", border="black")
+
+
+plot(y, residuals, main="Observed vs Residuals", xlab="Observed Values", ylab="Residuals", pch=19, col="blue")
+abline(h=0, col="red", lty=2)
