8ea77635e60e0223245311cd9f3f6411b7c9b3b2
christian-barbosa9 <cbarbosa603@icloud.com>
2024-12-14 16:55:28 -0500

    M_9.R

--- M_9.R
+++ M_9.R
@@ -3,3 +3,7 @@ contingency_table <- table(test$Class, test$satisfaction)
 chi_square_result <- chisq.test(contingency_table)
 print(chi_square_result)
 
+contingency_table <- table(test$Class, test$`On-board service`)
+
+chi_square_result <- chisq.test(contingency_table)
+print(chi_square_result)
